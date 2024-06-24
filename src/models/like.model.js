fe8797d85d5332113f92142543f6bc925663c539
Yash Bhavsar <yashbhavsar02@gmail.com>
2024-06-24 23:11:10 +0530

    Added comment, like, playlist, tweet models

---
+++ src/models/like.model.js
@@ -0,0 +1,25 @@
+import { Schema, model } from "mongoose";
+
+const likeSchema = new Schema(
+  {
+    video: {
+      type: Schema.Types.ObjectId,
+      ref: "Video",
+    },
+    likedBy: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    comment: {
+      type: Schema.Types.ObjectId,
+      ref: "Comment",
+    },
+    tweet: {
+      type: Schema.Types.ObjectId,
+      ref: "Tweet",
+    },
+  },
+  { timestamps: true }
+);
+
+export const Like = new model("Like", likeSchema);
---
+++ src/models/playlist.model.js
@@ -0,0 +1,27 @@
+import { Schema, model } from "mongoose";
+
+const playlistSchema = new Schema(
+  {
+    video: [
+      {
+        type: Schema.Types.ObjectId,
+        ref: "Video",
+      },
+    ],
+    owner: {
+      type: Schema.Types.ObjectId,
+      ref: "User",
+    },
+    name: {
+      type: String,
+      required: true,
+    },
+    description: {
+      type: String,
+      required: true,
+    },
+  },
+  { timestamps: true }
+);
+
+export const Playlist = new model("Playlist", playlistSchema);
